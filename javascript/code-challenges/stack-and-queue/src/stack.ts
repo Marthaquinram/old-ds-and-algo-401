59aaef4829fa9a8e155f9ff92443154be3829163
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-08-12 23:31:06 -0700

    started cc10

--- javascript/code-challenges/stack-and-queue/src/stack.ts
+++ javascript/code-challenges/stack-and-queue/src/stack.ts
@@ -1,17 +1,22 @@
 export class Stack<T> {
+  newArr: T[] = []; //new array to push thing into.
+
   get size(): number {
-    throw new Error("Not implemented");
+    return this.newArr.length; // this checking the length of the array
+    // throw new Error("Not implemented");
   }
 
   get peek(): T {
+    this.newArr[this.size - 1];
     throw new Error("Not implemented");
   }
 
-  push(t: T): void {
-    throw new Error("Not implemented");
+  push(value: T): void {
+    this.newArr.push(value);
   }
 
-  pop(): T {
-    throw new Error("Not implemented");
+  pop(): T | undefined {
+    return this.newArr.pop();
+    // throw new Error("Not implemented");
   }
 }
