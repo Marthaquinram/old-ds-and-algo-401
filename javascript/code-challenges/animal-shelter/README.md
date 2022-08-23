4cc7e6c62c96f435a762bb88c4da4cef7c69ddd8
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-08-22 22:03:15 -0700

    added read me

---
+++ javascript/code-challenges/animal-shelter/README.md
@@ -0,0 +1,12 @@
+# Challenge Summary
+
+Write code that will enqueue and dequeue animals
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
--- javascript/code-challenges/animal-shelter/src/shelter.ts
+++ javascript/code-challenges/animal-shelter/src/shelter.ts
@@ -19,39 +19,29 @@ export class Shelter {
     };
     this.animalArr.push(newAnimal);
   }
-  // If a cat or dog isn’t preferred, return whichever animal has been waiting in the shelter the longest.
+
   //isEmpty() — Returns true or false based on if the queue is empty or not
   //could use shift, removes the first element in an array and returns that removed element.
 
   dequeue(type?: "cat" | "dog"): any {
     // this is checking if queue is empty
-    // let savedAnimal: any;
     if (this.animalArr.length === 0) {
       throw Error("Queue is empty..");
-    }
-    // If no type is given (type === undefined), return the next animal.
-    // else {
-    if (this.animalArr.length) {
-      for (let i = 0; i < this.animalArr.length; i++) {
-        // If the type is given, return the next animal of a type.
-        if (this.animalArr[i].type === type) {
-          // return
-          return this.animalArr.splice(i, 1)[0].name;
+    } else {
+      if (this.animalArr.length) {
+        for (let i = 0; i < this.animalArr.length; i++) {
+          // If the type is given, return the next animal of a type.
+          if (this.animalArr[i].type === type) {
+            return this.animalArr.splice(i, 1)[0].name;
+          }
           // array.splice(index, howmany, item1, ....., itemX) - w3schools
-
-          // return this.animalArr[i].name;
-          // this.animalArr[i].name;
         }
       }
+      // If no type is given (type === undefined), return the next animal.
       if (type === undefined) {
         return this.animalArr.shift()?.name;
-        // return this.animalArr.shift()?.name; // whatever is shifted from this array we are saving it to savedAnimal
-        // return savedAnimal; //returned
-        // TODO(you);
-        //return; // next animal
+        //The shift() method removes the first element from an array and returns that removed element. - mdn
       }
     }
-    // return savedAnimal;
-    //     return null;
   }
 }
